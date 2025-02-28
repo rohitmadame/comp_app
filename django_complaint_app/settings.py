a1ee1470d116b1b728f9cc594aecf1ab93374869
unknown <rohitmadame2010@gmail.com>
2025-03-01 01:12:30 +0530

    Merge remote changes

--- django_complaint_app/settings.py
+++ django_complaint_app/settings.py
@@ -151,7 +151,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # ======================
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': False,
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
@@ -159,6 +158,6 @@ LOGGING = {
     },
     'root': {
         'handlers': ['console'],
-        'level': 'INFO',
+        'level': 'DEBUG' if DEBUG else 'INFO',
     },
 }
