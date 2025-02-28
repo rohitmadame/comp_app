b0278ceab77c1b44f8b219f589880c8b3ae96646
unknown <rohitmadame2010@gmail.com>
2025-02-28 11:43:47 +0530

    Trigger redeployment

--- comp_app/wsgi.py
+++ comp_app/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+import fcntl
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'comp_app.settings')
